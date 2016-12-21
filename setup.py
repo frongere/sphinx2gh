7250def267200b14de43a5fbd43d5950d7643af0
François Rongère <Francois.Rongere@ec-nantes.fr>
2016-12-21 18:04:50 +0100

    Adding skeleton of the project (licence, recipe) and an implementation of the tool (not working for the commit part

---
+++ setup.py
@@ -0,0 +1,51 @@
+"""Setup script for sphinx2gh."""
+from __future__ import print_function
+from setuptools import setup, find_packages
+import codecs
+import os
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """Return multiple read calls to different readable objects as a single
+    string."""
+    # intentionally *not* adding an encoding option to open
+    return codecs.open(os.path.join(HERE, *parts), 'r').read()
+
+LONG_DESCRIPTION = read('README.rst')
+
+setup(
+    name='sphinx2gh',
+    version='0.1',
+    url='https://',  # TODO : A completer
+    author='Francois Rongere',
+    author_email='franrongere@gmail.com',
+    description="""A command line tool to deploy Sphinx documentation to GitHub""",
+    long_description=LONG_DESCRIPTION,
+    license='BSD',
+    keywords='Documentation, Sphinx, Git, GitHub',
+    packages=find_packages(exclude=['contrib', 'doc', 'tests*']),
+    # setup_requires=['pytest-runner'],
+    # tests_require=['pytest', 'pytest-cov'],
+    install_requires=['gitpython', 'argcomplete'],
+    entry_points={
+        'console_scripts': [
+            'sphinx2gh=sphinx2gh:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Software Development :: Version Control',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
+)
